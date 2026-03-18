884b966e22fdd2979c4de51877ba47f0032a3e38
Gianluca Foglia <g.foglia1996@gmail.com>
2026-03-18 19:25:59 +0100

    Modello e migrazione,relazioni,article create,salvataggio in database

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+class ArticleController extends Controller
+{
+    public function create()
+    {
+        return view('article.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+
+Route::get('/create/article', [ArticleController::class, 'create'])->name('create.article');
 
 /* Route::get('/', function () {
     return view('welcome');
