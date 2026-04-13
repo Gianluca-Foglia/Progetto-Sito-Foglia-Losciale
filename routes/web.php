169c836f20f788297fca7ee2f6b71e02c1d8446d
Gianluca Foglia <g.foglia1996@gmail.com>
2026-04-13 18:43:28 +0200

    Creazione del ruolo Revisore,Creazione di un comando di automazione,Gestione dello stato degli articoli,Dashboard dei Revisori,Logica di valutazione articolo

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\RevisorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
@@ -22,3 +23,7 @@ Route::get('/show/article/{article}', [ArticleController::class, 'show'])->name(
 // rotta parametrica per le categorie
 
 Route::get('/category/{category}', [ArticleController::class, 'byCategory'])->name('byCategory');
+
+/* rotta parametrica per i Revisor */
+
+Route::get('/revisor/index', [RevisorController::class, 'index'])->name('revisor.index');
